e055d5275fbaac0d47c0911b73bbde4ad5c44500
Rory O'Connor <7roconnor@stpatrick.org>
2017-04-06 13:56:35 -0500

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,1 +1,22 @@
-const doc = document;
+(function(){
+  const doc = document;
+
+  let article = doc.querySelector('article');
+
+  let isWhite = false;
+
+  article.addEventListener('click', function(){
+    if(isWhite == true){
+      article.style.border = '20px solid green';
+      isWhite = false;
+    }
+    else {
+      article.style.border = '20px solid white';
+      isWhite = true;
+    }
+  });
+
+
+
+
+})();
